8ce8dbd464299c369add03db278b84e62db680c8
Tal Einat <532281+taleinat@users.noreply.github.com>
2025-05-24 22:35:04 +0300

    Upgrade to tox 4 and fix installing deps in GHA
    Also upgrade actions and hopefully get some Python dependency caching working.

--- tox_install_deps.py
+++ tox_install_deps.py
@@ -4,11 +4,11 @@ import sys
 
 if __name__ == "__main__":
     args = sys.argv[1:]
-    if "--no-deps" not in args:
+    if "--no-deps" in args:
+        subprocess.run(["flit", "install", "--deps", "none"], check=True)
+    else:
         subprocess.run(
             ["pip", "install", "--disable-pip-version-check", "--no-python-version-warning"] + args,
             check=True,
         )
         subprocess.run(["flit", "install", "--extras", "test"], check=True)
-    else:
-        subprocess.run(["flit", "install", "--deps", "none"], check=True)
