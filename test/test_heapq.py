60b6ede6c66643cf1f5d88d4ed7b2e6ade838e49
Tal Einat <532281+taleinat@users.noreply.github.com>
2025-05-24 23:27:54 +0300

    Lint

--- test/test_heapq.py
+++ test/test_heapq.py
@@ -4,7 +4,7 @@ def test_nlargest(Chain):
     assert Chain([1, 3, 2, 4, 3]).nlargest(2).value == [4, 3]
     assert Chain([1, 3, 2, 4, 3]).nlargest(3).value == [4, 3, 3]
     assert Chain([1, 3, 2, 4, 1]).nlargest(0).value == []
-    assert Chain([1, 3, 2, 4, 3]).nlargest(1, key=lambda x:x % 3).value == [2]
+    assert Chain([1, 3, 2, 4, 3]).nlargest(1, key=lambda x: x % 3).value == [2]
 
 
 def test_nsmallest(Chain):
@@ -13,4 +13,4 @@ def test_nsmallest(Chain):
     assert Chain([1, 3, 2, 4, 1]).nsmallest(2).value == [1, 1]
     assert Chain([1, 3, 2, 4, 1]).nsmallest(3).value == [1, 1, 2]
     assert Chain([1, 3, 2, 4, 1]).nsmallest(0).value == []
-    assert Chain([1, 3, 2, 4, 3]).nsmallest(2, key=lambda x:x % 3).value == [3, 3]
+    assert Chain([1, 3, 2, 4, 3]).nsmallest(2, key=lambda x: x % 3).value == [3, 3]
